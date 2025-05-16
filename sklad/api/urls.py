f1df4a2617e3d69ed5f06f7b56218587e83f3b34
Николай С <nicksrnk@gmail.com>
2025-05-16 22:06:35 +0500

    Доработан новый отчет о потреблении

--- sklad/api/urls.py
+++ sklad/api/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from .views import generate_user_report_xlsx
+from .views import generate_user_report_xlsx, generate_second_report_xlsx
 
 urlpatterns = [
     path('report/', generate_user_report_xlsx),
+    path('report2/', generate_second_report_xlsx),
 ]
--- sklad/api/views.py
+++ sklad/api/views.py
@@ -3,7 +3,7 @@ import pandas as pd
 from django.http import HttpResponse
 from rest_framework.decorators import api_view
 
-from .models import Inventory
+from .models import Inventory, Remain, Material
 
 
 @api_view(['GET'])
@@ -16,8 +16,31 @@ def generate_user_report_xlsx(request):
     data.append({
         'Номер': inventory.id,
         'Сотрудник проводивший инвентаризацию': inventory.employee.fio,
+        'Материал': inventory.materials.all()[0],
         'Учетное количество': inventory.count,
     })
+    df = pd.DataFrame(data)
+    output = io.BytesIO()
+    with pd.ExcelWriter(output, engine='xlsxwriter') as writer:
+        df.to_excel(writer, sheet_name='Report', index=False)
+    output.seek(0)
+    response = HttpResponse(
+        output.read(),
+        content_type="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
+    )
+    response['Content-Disposition'] = 'attachment; filename=report.xlsx'
+    response['Content-Transfer-Encoding'] = 'binary'
+    return response
+
+@api_view(['GET'])
+def generate_second_report_xlsx(request):
+    data = []
+    for m in Material.objects.all():
+        data.append({
+            'Материал': m.name,
+            'Учетное количество': m.number,
+        })
+
     df = pd.DataFrame(data)
     output = io.BytesIO()
     with pd.ExcelWriter(output, engine='xlsxwriter') as writer:
