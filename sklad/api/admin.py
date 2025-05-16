6175b5b4e077ea6261acecff4d75ea9267747a57
Николай С <nicksrnk@gmail.com>
2025-05-16 22:11:37 +0500

    Добавлены недостающие модели в админку

--- sklad/api/admin.py
+++ sklad/api/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from .models import Supplier, Employee, Supply, Material, Inventory
+from .models import Supplier, Employee, Supply, Material, Inventory, Remain, Response
+
 admin.site.site_header = "Даркстор схема"
 
 
@@ -28,4 +29,12 @@ class MaterialAdmin(admin.ModelAdmin):
 
 @admin.register(Inventory)
 class InventoryAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Remain)
+class RemainAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Response)
+class ResponseAdmin(admin.ModelAdmin):
     pass
